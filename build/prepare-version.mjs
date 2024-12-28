857a34884425af4adcd995ed479f385d15899889
Julien Déramond <juderamond@gmail.com>
2024-12-28 15:31:28 +0100

    fix: `npm run lint`

--- build/prepare-version.mjs
+++ build/prepare-version.mjs
@@ -1,29 +1,38 @@
+#!/usr/bin/env node
+
 // Get the current version from package.json and increment the version based on the argument passed to the script.
 // Arguments: patch, minor, major
 
-import { readFileSync } from 'fs'
-import { execSync } from 'child_process'
+import { readFileSync } from 'node:fs'
+import { execSync } from 'node:child_process'
 
 function prepare(semver) {
   const packageJson = JSON.parse(readFileSync('package.json', 'utf8'))
-  const version = packageJson.version
+  const { version } = packageJson
   const [major, minor, patch] = version.split('.').map(Number)
 
-  let newVersion = version;
-  
+  let newVersion = version
+
   switch (semver) {
-    case 'patch':
+    case 'patch': {
       newVersion = `${major}.${minor}.${patch + 1}`
       break
-    case 'minor':
+    }
+
+    case 'minor': {
       newVersion = `${major}.${minor + 1}.0`
       break
-    case 'major':
+    }
+
+    case 'major': {
       newVersion = `${major + 1}.0.0`
       break
-    default:
+    }
+
+    default: {
       console.error(`Unknown semver "${semver}"`)
       process.exit(1)
+    }
   }
 
   execSync(`node build/change-version.mjs ${version} ${newVersion}`)
